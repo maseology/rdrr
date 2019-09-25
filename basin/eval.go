6e55601866dae82d7b8d93c8f84a07c15c0ab83d
maseology <mason@riffle.ca>
2019-09-25 14:34:19 -0400

    update 190925

--- basin/eval.go
+++ basin/eval.go
@@ -2,7 +2,6 @@ package basin
 
 import (
 	"fmt"
-	"log"
 	"math"
 	"sync"
 )
@@ -20,65 +19,63 @@ type stran struct {
 func (b *subdomain) eval(p *sample, Ds, m float64, print bool) (of float64) {
 	var wg sync.WaitGroup
 	dt, y, ep, obs, intvl, nstep := b.getForcings()
-	if print {
-		if len(b.swsord) == 1 {
-			rs := newResults(b, intvl, nstep)
-			rs.dt, rs.obs = dt, obs
-			var res resulter = &rs
-			pp := newSubsample(b, p, Ds, m, -1)
-			pp.y, pp.ep, pp.nstep = y, ep, nstep
-			pp.eval(Ds, m, res)
-			res.report()
-		} else {
-			transfers := make(map[int][]itran, len(b.rtr.swscidxr))
-			nrnds := len(b.swsord)
-			for i, k := range b.swsord {
-				fmt.Printf("--> round %d (of %d): %d sws\n", i+1, nrnds, len(k))
-				chstrans := make(chan stran, len(k))
-				for _, sid := range k {
-					wg.Add(1)
-					dsid := -1
-					if d, ok := b.rtr.dsws[sid]; ok {
-						transfers[d] = []itran{}
-						dsid = d
-					}
-					go func(sid int) {
-						defer wg.Done()
-						pp := newSubsample(b, p, Ds, m, sid)
-						pp.y, pp.ep, pp.nstep = y, ep, nstep
-						if t, ok := transfers[sid]; ok {
-							pp.in = make(map[int][]float64, len(t)) // upstream inputs
-							for _, v := range t {
-								pp.in[pp.xr[v.c]] = v.v
-							}
-						}
-						if sid == b.cid0 { // outlet
-							rs := newResults(b, intvl, nstep)
-							rs.dt, rs.obs = dt, obs
-							var res resulter = &rs
-							fmt.Printf(" printing SWS %d\n\n", sid)
-							pp.eval(Ds, m, res)
-							res.report()
-						} else {
-							var res resulter = &outflow{}
-							fmt.Printf(" running SWS %d\n", sid)
-							pp.eval(Ds, m, res)
-							chstrans <- stran{s: dsid, i: itran{c: b.ds[sid], v: res.report()}}
-						}
-					}(sid)
-				}
-				wg.Wait()
-				close(chstrans)
-				for t := range chstrans {
-					transfers[t.s] = append(transfers[t.s], t.i)
-				}
-			}
-		}
+	if len(b.swsord) == 1 {
+		rs := newResults(b, intvl, nstep)
+		rs.dt, rs.obs = dt, obs
+		var res resulter = &rs
+		pp := newSubsample(b, p, Ds, m, -1)
+		pp.y, pp.ep, pp.nstep = y, ep, nstep
+		pp.eval(Ds, m, res)
+		res.report(print)
 	} else {
-		if len(b.swsord) > 0 {
-			log.Fatalf(" b.eval todo2\n")
-		} else {
-			log.Fatalf(" b.eval todo3\n")
+		transfers := make(map[int][]itran, len(b.rtr.swscidxr))
+		nrnds := len(b.swsord)
+		for i, k := range b.swsord {
+			if print {
+				fmt.Printf("--> round %d (of %d): %d sws\n", i+1, nrnds, len(k))
+			}
+			chstrans := make(chan stran, len(k))
+			for _, sid := range k {
+				wg.Add(1)
+				dsid := -1
+				if d, ok := b.rtr.dsws[sid]; ok {
+					transfers[d] = []itran{}
+					dsid = d
+				}
+				go func(sid int) {
+					defer wg.Done()
+					pp := newSubsample(b, p, Ds, m, sid)
+					pp.y, pp.ep, pp.nstep = y, ep, nstep
+					if t, ok := transfers[sid]; ok {
+						pp.in = make(map[int][]float64, len(t)) // upstream inputs
+						for _, v := range t {
+							pp.in[pp.xr[v.c]] = v.v
+						}
+					}
+					if sid == b.cid0 { // outlet
+						rs := newResults(b, intvl, nstep)
+						rs.dt, rs.obs = dt, obs
+						var res resulter = &rs
+						if print {
+							fmt.Printf(" printing SWS %d\n\n", sid)
+						}
+						pp.eval(Ds, m, res)
+						of = res.report(print)[0]
+					} else {
+						var res resulter = &outflow{}
+						if print {
+							fmt.Printf(" running SWS %d\n", sid)
+						}
+						pp.eval(Ds, m, res)
+						chstrans <- stran{s: dsid, i: itran{c: b.ds[sid], v: res.report(false)}}
+					}
+				}(sid)
+			}
+			wg.Wait()
+			close(chstrans)
+			for t := range chstrans {
+				transfers[t.s] = append(transfers[t.s], t.i)
+			}
 		}
 	}
 	return
--- basin/results.go
+++ basin/results.go
@@ -9,13 +9,13 @@ import (
 )
 
 type resulter interface {
-	report() []float64
+	report(bool) []float64
 	getTotals(sim, bf []float64, ytot, atot, rtot, gtot, btot float64)
 }
 
 type outflow struct{ sim []float64 }
 
-func (o *outflow) report() []float64                                          { return o.sim }
+func (o *outflow) report(dummy bool) []float64                                { return o.sim }
 func (o *outflow) getTotals(sim, dummy []float64, d0, d1, d2, d3, d4 float64) { o.sim = sim }
 
 type results struct {
@@ -40,20 +40,22 @@ func (r *results) getTotals(sim, bf []float64, ytot, atot, rtot, gtot, btot floa
 	r.sim, r.bf = sim, bf
 	r.ytot, r.atot, r.rtot, r.gtot, r.btot = ytot, atot, rtot, gtot, btot
 }
-func (r *results) report() []float64 {
+func (r *results) report(print bool) []float64 {
 	for k := 0; k < r.nstep; k++ {
 		r.sim[k] *= r.h2cms / r.fncid
 		r.bf[k] *= r.h2cms / r.fnstrm / r.fncid
 	}
 	rmse := objfunc.RMSE(r.obs[365:], r.sim[365:])
-	// sumWriteRMaps(b.mdldir, xr, ds, gy, ga, gr, gg, gd, gl, float64(nstep))
-	kge := objfunc.KGE(r.obs, r.sim)
-	mwr2 := objfunc.Krause(computeMonthly(r.dt[365:], r.obs[365:], r.sim[365:], float64(r.intvl), r.contarea))
-	nse := objfunc.NSE(r.obs, r.sim)
-	bias := objfunc.Bias(r.obs, r.sim)
-	ff := 365.24 * 1000. / float64(r.nstep) / r.fncid
-	fmt.Printf("  waterbudget [mm/yr]: pre: %.0f  aet: %.0f  rch: %.0f  gwd: %.0f  olf: %.0f  dif: %.1f\n", r.ytot*ff, r.atot*ff, r.gtot*ff, r.btot*ff, r.rtot*ff, (r.ytot+r.btot-(r.atot+r.gtot+r.rtot))*ff)
-	fmt.Printf("  KGE: %.3f  NSE: %.3f  RMSE: %.3f  mon-wR²: %.3f  Bias: %.3f\n", kge, nse, rmse, mwr2, bias)
-	mmio.ObsSim("hyd.png", r.obs, r.sim, r.bf, nil)
+	if print {
+		// sumWriteRMaps(b.mdldir, xr, ds, gy, ga, gr, gg, gd, gl, float64(nstep))
+		kge := objfunc.KGE(r.obs, r.sim)
+		mwr2 := objfunc.Krause(computeMonthly(r.dt[365:], r.obs[365:], r.sim[365:], float64(r.intvl), r.contarea))
+		nse := objfunc.NSE(r.obs, r.sim)
+		bias := objfunc.Bias(r.obs, r.sim)
+		ff := 365.24 * 1000. / float64(r.nstep) / r.fncid
+		fmt.Printf("  waterbudget [mm/yr]: pre: %.0f  aet: %.0f  rch: %.0f  gwd: %.0f  olf: %.0f  dif: %.1f\n", r.ytot*ff, r.atot*ff, r.gtot*ff, r.btot*ff, r.rtot*ff, (r.ytot+r.btot-(r.atot+r.gtot+r.rtot))*ff)
+		fmt.Printf("  KGE: %.3f  NSE: %.3f  RMSE: %.3f  mon-wR²: %.3f  Bias: %.3f\n", kge, nse, rmse, mwr2, bias)
+		mmio.ObsSim("hyd.png", r.obs, r.sim, r.bf, nil)
+	}
 	return []float64{rmse}
 }
