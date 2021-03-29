c5341063b9187539e64bff836172aa0895896c84
maseology <mason@riffle.ca>
2021-03-29 09:49:42 -0400

    02HB004

--- run/main.go
+++ run/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"runtime"
 
 	"github.com/maseology/mmio"
@@ -11,11 +12,12 @@ import (
 func main() {
 
 	const (
-		mdlPrfx = "S:/OWRC-RDRR/owrc."
-		// mdlPrfx = "M:/Peel/RDRR-PWRMM21/PWRMM21."            //"S:/Peel/PWRMM21." //
-		// obsfp   = "M:/Peel/RDRR-PWRMM21/dat/obs/02HB004.csv" // "S:/Peel/obs/02HB008.csv" //"M:/Peel/RDRR-PWRMM21/dat/obs/02HB008.csv" //
-		cid0 = -1 // 2014386                                    // 1552736
+		// mdlPrfx = "S:/OWRC-RDRR/owrc."
+		mdlPrfx = "M:/Peel/RDRR-PWRMM21/PWRMM21." //"S:/Peel/PWRMM21." //
+		obsfp   = "S:/Peel/obs/02HB004.csv"       //"M:/Peel/RDRR-PWRMM21/dat/obs/02HB004.csv" // "S:/Peel/obs/02HB008.csv" //"M:/Peel/RDRR-PWRMM21/dat/obs/02HB008.csv" //
+		cid0    = 2014386                         //-1                                         //  1552736
 	)
+	//02HB024
 
 	fmt.Println("")
 	tt := mmio.NewTimer()
@@ -26,9 +28,9 @@ func main() {
 	tt.Print("Master Domain Load complete\n")
 
 	model.DeleteMonitors(mdlPrfx+"out/", true) // also sets-up the output folder
-	// if err := dom.Frc.AddObservation(obsfp, dom.Strc.Acell, cid0); err != nil {
-	// 	log.Fatalln(err)
-	// }
+	if err := dom.Frc.AddObservation(obsfp, dom.Strc.Acell, cid0); err != nil {
+		log.Fatalln(err)
+	}
 
 	// fobs := dom.Frc.O[0]
 	// oobs, _ := postpro.GetObservations("C:/Users/Mason/Desktop/", "")
@@ -47,32 +49,32 @@ func main() {
 	// _ = cobs
 	// fmt.Println("")
 
-	// // // run model
-	// // // TMQm := 1.
-	// // // grdMin := .01
-	// // // kstrm := .995
-	// // // mcasc := 1. // .001-10
-	// // // soildepth := .815
-	// // // kfact := 1.
-	// // // dinc := 10.5
-	// // TMQm := .633454
-	// // grdMin := .000379
-	// // kstrm := .954231
-	// // mcasc := .211821 // .001-10
-	// // soildepth := 1.298047
-	// // kfact := .002853
-	// // dinc := 1.114309
-	// TMQm := 3.87
-	// grdMin := .007215
-	// kstrm := .9959
-	// mcasc := .377441 // .001-10
-	// soildepth := .164
-	// kfact := .009681
-	// dinc := 1.5
-	// fmt.Println(dom.RunDefault(mdlPrfx+"out/", mdlPrfx+"check/", TMQm, grdMin, kstrm, mcasc, soildepth, kfact, dinc, cid0, true))
-	// // fmt.Println(model.RunDefault(mdlPrfx+"out/", mdlPrfx+"check/", 37.866772, 2.60e-05, 0.64884, 0.002168, 1.374418, 0.020174, 4.654649, 1552736, true))
-	// TMQm, grdMin, kstrm, mcasc, soildepth, dinc := 1.654184, 0.000266, 0.997622, 0.306692, 0.563079, 1.770998
-	// ksat := []float64{1.762202e-10, 1.881964e-08, 4.421245e-07, 2.033684e-04, 5.739264e-04, 6.942467e-05, 5.528772e-08, 3.740342e-06}
+	// // run model
+	// // // // TMQm := 1.
+	// // // // grdMin := .01
+	// // // // kstrm := .995
+	// // // // mcasc := 1. // .001-10
+	// // // // soildepth := .815
+	// // // // kfact := 1.
+	// // // // dinc := 10.5
+	// // // TMQm := .633454
+	// // // grdMin := .000379
+	// // // kstrm := .954231
+	// // // mcasc := .211821 // .001-10
+	// // // soildepth := 1.298047
+	// // // kfact := .002853
+	// // // dinc := 1.114309
+	// // TMQm := 3.87
+	// // grdMin := .007215
+	// // kstrm := .9959
+	// // mcasc := .377441 // .001-10
+	// // soildepth := .164
+	// // kfact := .009681
+	// // dinc := 1.5
+	// // fmt.Println(dom.RunDefault(mdlPrfx+"out/", mdlPrfx+"check/", TMQm, grdMin, kstrm, mcasc, soildepth, kfact, dinc, cid0, true))
+	// // // fmt.Println(model.RunDefault(mdlPrfx+"out/", mdlPrfx+"check/", 37.866772, 2.60e-05, 0.64884, 0.002168, 1.374418, 0.020174, 4.654649, 1552736, true))
+	// TMQm, grdMin, kstrm, mcasc, soildepth, dinc := 0.208767, 0.000368, 0.985471, 0.235153, 0.4125, 0.264875
+	// ksat := []float64{1.42e-07, 4.00e-09, 1.06e-07, 3.43e-05, 0.006156758, 1.23e-09, 4.64e-05, 2.27e-08}
 	// fmt.Println(dom.RunSurfGeo(mdlPrfx+"out/", mdlPrfx+"check/", TMQm, grdMin, kstrm, mcasc, soildepth, dinc, ksat, cid0, true))
 
 	// sample models
